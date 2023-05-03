10552a8fa36c7021c10a916821e4ac6166fb0d1d
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-05-03 21:06:26 +0200

    ADD - AboutPage

---
+++ src/_components/layout/About/AboutPage.tsx
@@ -0,0 +1,34 @@
+import text from "domain/texts/About/about.json";
+
+import styles from "./AboutPage.module.css";
+import Navigation from "_components/Navigation";
+import Jobs from "./Jobs";
+
+export const AboutPage = () => {
+  return (
+    <main>
+      <h1 className={styles.title}>{text.title}</h1>
+      <div className={styles.navigationContainer}>
+        <Navigation />
+      </div>
+      <div className={styles.jobsContainer}>
+        {text.experience.map((experience) => (
+          <Jobs
+            key={experience.company}
+            company={experience.company}
+            dates={experience.dates}
+            description={experience.description}
+            role={experience.role}
+          />
+        ))}
+      </div>
+      {/* <div className={styles.textContainer}>
+        {text.content.map((contentText) => (
+          <p className={styles.text} key={contentText}>
+            {contentText}
+          </p>
+        ))}
+      </div> */}
+    </main>
+  );
+};
---
+++ src/app/about/page.tsx
@@ -0,0 +1,5 @@
+import AboutPage from "_components/layout/About";
+
+export default function About() {
+  return <AboutPage />;
+}
