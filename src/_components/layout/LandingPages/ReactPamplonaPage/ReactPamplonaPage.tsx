0d4bbee27abea800d7a03286802a08afcbf38a8b
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-05-07 17:02:46 +0200

    ADD - Desktop styles

--- src/_components/layout/LandingPages/ReactPamplonaPage/ReactPamplonaPage.tsx
+++ src/_components/layout/LandingPages/ReactPamplonaPage/ReactPamplonaPage.tsx
@@ -10,26 +10,33 @@ import styles from "./ReactPamplonaPage.module.css";
 export const ReactPamplonaPage = () => {
   return (
     <main>
-      <div className={styles.navigationContainer}>
-        <Navigation />
-      </div>
-      <h1 className={styles.title}>{text.name}</h1>
-      <h2 className={styles.subtitle}>{text.profile}</h2>
-      <h3 className={styles.location}>{text.location}</h3>
-      <div>
-        <p className={styles.text}>{text.experience}</p>
-        {text.strengths.map((strength) => (
-          <p className={styles.text} key={strength}>
-            {strength}
-          </p>
-        ))}
-        <p className={styles.text}>{text.availability}</p>
-        <Link className={styles.contact} href={`mailto: ${text.contact.email}`}>
-          {text.contact.email}
-        </Link>
-      </div>
-      <div className={styles.socialMediaContainer}>
-        <SocialMedia />
+      <div className={styles.container}>
+        <div className={styles.navigationContainer}>
+          <Navigation />
+        </div>
+        <div className={styles.headers}>
+          <h1 className={styles.title}>{text.name}</h1>
+          <h2 className={styles.subtitle}>{text.profile}</h2>
+          <h3 className={styles.location}>{text.location}</h3>
+        </div>
+        <div className={styles.textContainer}>
+          <p className={styles.text}>{text.experience}</p>
+          {text.strengths.map((strength) => (
+            <p className={styles.text} key={strength}>
+              {strength}
+            </p>
+          ))}
+          <p className={styles.text}>{text.availability}</p>
+          <Link
+            className={styles.contact}
+            href={`mailto: ${text.contact.email}`}
+          >
+            {text.contact.email}
+          </Link>
+        </div>
+        <div className={styles.socialMediaContainer}>
+          <SocialMedia />
+        </div>
       </div>
     </main>
   );
