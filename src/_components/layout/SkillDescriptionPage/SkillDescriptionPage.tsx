30fde4391462f4bbd6e043c30790be91ed877ebe
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-05-13 15:12:21 +0200

    ADD - SkillsDescription texts

--- src/_components/layout/SkillDescriptionPage/SkillDescriptionPage.tsx
+++ src/_components/layout/SkillDescriptionPage/SkillDescriptionPage.tsx
@@ -4,25 +4,53 @@ import Navigation from "_components/Navigation";
 
 import styles from "./SkillDescriptionPage.module.css";
 
+import skillsDescriptions from "domain/texts/Skills/SkillsDescription/SkillsDescription.json";
+
 interface Props {
   skillName: string;
 }
 export const SkillDescriptionPage = ({ skillName }: Props) => {
+  const skillDescription = skillsDescriptions.filter(
+    (skillsD) => skillsD.SkillName === skillName
+  )[0];
+
   return (
     <main>
       <div>
         <Navigation />
       </div>
       <h1 className={styles.title}>{skillName}</h1>
-      <h2 className={styles.subtitle}>Work Experience</h2>
-      <p>Biko</p>
-      <h2 className={styles.subtitle}>Projects</h2>
-      <p>Ibarra Photo</p>
+      <div className={styles.fieldsContainer}>
+        <div>
+          <h2 className={styles.subtitle}>Work Experience</h2>
+          {skillDescription.workExperience?.map((experience) => (
+            <p key={experience} className={styles.description}>
+              {experience}
+            </p>
+          ))}
+        </div>
+        <div>
+          <h2 className={styles.subtitle}>Projects</h2>
+          {skillDescription.projects?.map((project) => (
+            <p key={project} className={styles.description}>
+              {project}
+            </p>
+          ))}
+        </div>
 
-      <h2 className={styles.subtitle}>Certificates</h2>
-      <p>Free Code Camp</p>
+        <div>
+          <h2 className={styles.subtitle}>Certificates</h2>
+          {skillDescription.certificates?.map((certificate) => (
+            <p key={certificate} className={styles.description}>
+              {certificate}
+            </p>
+          ))}
+        </div>
+      </div>
 
-      <Link href={"/skills"}>Go Back</Link>
+      <Link className={styles.backButton} href={"/skills"}>
+        Go Back
+      </Link>
     </main>
   );
 };
