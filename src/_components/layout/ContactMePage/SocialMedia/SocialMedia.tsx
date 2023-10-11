c5b4ca9d407af5bf560484999b6e4c86d38ceb94
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-10-11 19:37:51 +0200

    ADD - Social media icons hover action

--- src/_components/layout/ContactMePage/SocialMedia/SocialMedia.tsx
+++ src/_components/layout/ContactMePage/SocialMedia/SocialMedia.tsx
@@ -7,7 +7,7 @@ import styles from "./SocialMedia.module.css";
 export const SocialMedia = () => {
   return (
     <div className={styles.socialMediaContainer}>
-      <div>
+      <div className={styles.socialMediaLink}>
         <Link
           href={"https://github.com/aitor-i"}
           target="_blank"
@@ -16,7 +16,7 @@ export const SocialMedia = () => {
           <FaGithub size={"3rem"} />
         </Link>
       </div>
-      <div>
+      <div className={styles.socialMediaLink}>
         <Link
           href={"https://twitter.com/aitori9_i"}
           target="_blank"
@@ -25,8 +25,12 @@ export const SocialMedia = () => {
           <FaTwitter size={"3rem"} />
         </Link>
       </div>
-      <div>
-        <Link href={"https://www.linkedin.com/in/aitor-i-5b1b1b1b2/"}>
+      <div className={styles.socialMediaLink}>
+        <Link
+          href={"https://www.linkedin.com/in/aitor-i-5b1b1b1b2/"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FaLinkedinIn size={"3rem"} />
         </Link>
       </div>
