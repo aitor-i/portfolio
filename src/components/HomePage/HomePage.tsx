d211b4264a9ae90b84c900260a9f4b86aaacd861
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-01 11:11:39 +0200

    ADD - Home page

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,9 @@
+
+export default function HomePage() {
+  return (
+    <section className="px-4 flex justify-center items-start flex-col h-screen md:px-12">
+      <h1 className="text-8xl md:text-9xl">Aitor Ibarra</h1>
+      <h2 className="text-5xl">Developer</h2>
+    </section>
+  )
+}
