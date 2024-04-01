c5e97e75a4f56d2234b0d09eab5b6518eaaaaa43
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-01 11:20:37 +0200

    ADD - header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <nav className="grid grid-cols-4 md:grid-cols-8 px-4 py-4 md:px-8 gap-4">
+      <span>
+        <Link href={"/"}>Aitor Ibarra</Link>
+      </span>
+      <Link className="col-start-7" href={"/"}>Projects</Link>
+      <Link className="col-start-8" href={"/"}>Blog</Link>
+    </nav>
+  )
+}
