28ec006af34283bc1460d5868856cc2743dff268
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-01 20:35:58 +0200

    ADD -Blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <section className="px-4 md:px-8 h-screen" >
+      <h1 className="text-5xl md:text-6xl">Coming Soon</h1>
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-3 py-4 md:py-8">
+        <h3>No blog posts yet...</h3>
+      </div>
+    </section>
+
+  )
+}
