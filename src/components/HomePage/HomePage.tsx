0ce67ba9c3954291b679439d60593146e8d59b82
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-01 15:34:30 +0200

    UPDATE - header stles

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,7 +1,7 @@
 
 export default function HomePage() {
   return (
-    <section className="px-4 flex justify-center items-start flex-col h-screen md:px-12">
+    <section className="px-4 flex py-20 md:py-52 items-start flex-col h-screen md:px-12">
       <h1 className="text-8xl md:text-9xl">Aitor Ibarra</h1>
       <h2 className="text-5xl">Developer</h2>
     </section>
