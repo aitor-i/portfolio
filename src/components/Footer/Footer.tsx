84cca3ddc9f58fb2fb78498014b4948702d7faef
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-01 20:36:56 +0200

    UPDATE - Footer links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,19 @@
 import Link from "next/link"
-import { FacebookIcon, InstagramIcon, TwitterIcon } from "../Icons/Icons"
+import { FacebookIcon, GithubIcon, InstagramIcon, LinkedinIcon, MailIcon, TwitterIcon } from "../Icons/Icons"
 export default function Footer() {
   return (
-    <footer className="p-6 text-white">
+    <footer className="p-6 bg-gray-950 text-white">
       <div className="flex items-center justify-between">
         <p>© 2024 Aitor Ibarra. All rights reserved.</p>
         <div className="space-x-4">
-          <Link href="#">
-            <FacebookIcon className="h-6 w-6" />
+          <Link href="mailto:contact@aitoribarra.com">
+            <MailIcon className="h-6 w-6" />
           </Link>
-          <Link href="#">
-            <TwitterIcon className="h-6 w-6" />
+          <Link href="https://www.linkedin.com/in/aitor-i-5b1b1b1b2/">
+            <LinkedinIcon className="h-6 w-6" />
           </Link>
-          <Link href="#">
-            <InstagramIcon className="h-6 w-6" />
+          <Link href="https://github.com/aitor-i">
+            <GithubIcon className="h-6 w-6" />
           </Link>
         </div>
       </div>
