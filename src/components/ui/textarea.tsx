59d08ce1abc8e8e9882ade29ac83c22b05f4d3a0
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-03 21:54:14 +0200

    UPDATE - Input and test area styles

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -7,7 +7,7 @@ export const Textarea =
   ({ className, ...props }: TextareaProps) => {
     return (
       <textarea
-        className={"flex h-10 w-full rounded-md border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-500 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 " + className}
+        className={"flex text-gray-950 h-10 w-full rounded-md border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-500 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 " + className}
         {...props}
       />
     )
