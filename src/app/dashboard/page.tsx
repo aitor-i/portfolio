9674a7195950e1de6188a1c5ab4f7b3def97404e
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-03 19:05:20 +0200

    ADD - Dasboard page route

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import Dashboard from "@/components/Dashboard/Dashboard";
+import Login from "@/components/Login/Login";
+
+export default function page() {
+  return (
+    <>
+      <Login />
+      <Dashboard />
+    </>
+  )
+}
