5d4747f3037b7f7555c3e23d82eda6d68be01e24
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-03 21:54:03 +0200

    ADD - Upload post

--- src/components/BlogForm/BlogForm.tsx
+++ src/components/BlogForm/BlogForm.tsx
@@ -1,45 +1,29 @@
 'use client'
 
-import React from 'react'
+import React, { useRef } from 'react'
 import { Input } from "@/components/ui/input"
 import { Textarea } from '../ui/textarea'
 import { Button } from "@/components/ui/button"
+import { postBlog } from '@/server-actions/blog/blog'
 
 export function BlogForm() {
 
+  const formRef = useRef<HTMLFormElement>(null)
 
   const onSubmitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
+
     const formData = new FormData(event.currentTarget)
-    const name = formData.get('name')?.valueOf();
-    const surname = formData.get('surname')?.valueOf();
-    const email = formData.get('email')?.valueOf();
-    const phone = formData.get('phone')?.valueOf();
-    const message = formData.get('message')?.valueOf();
 
-    const body = {
-      name,
-      surname,
-      email,
-      phone,
-      message
-    }
-
-    const url = new URL('http://localhost:3003/api/contact-form');
+    const title = formData.get('title')?.toString()!
+    const post = formData.get('post')?.toString()!
+    formRef.current?.reset()
 
 
-    const response = await fetch(url, {
-      method: 'POST', // Set the method to POST
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body), // Convert the data to a JSON string
-    });
+    const res = await postBlog(title, post)
 
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
+    console.log(res.acknowledged)
 
   }
 
@@ -47,9 +31,9 @@ export function BlogForm() {
   return (
     <div className="mt-8">
       <h2 className="text-3xl font-bold mb-4 tracking-tight">Post a blog entry</h2>
-      <form className="grid gap-4" onSubmit={onSubmitHandler}>
-        <Input name='title' placeholder="Title" />
-        <Textarea name='post' className="min-h-[10rem]" placeholder="Blog post" />
+      <form className="grid gap-4" ref={formRef} onSubmit={onSubmitHandler}>
+        <Input name='title' required placeholder="Title" />
+        <Textarea name='post' required className="min-h-[10rem]" placeholder="Blog post" />
         <Button className='bg-blue-500 hover:bg-blue-600' type="submit">Upload</Button>
       </form>
     </div>
