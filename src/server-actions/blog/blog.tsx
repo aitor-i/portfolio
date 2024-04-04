9c91e5482be6339a2b45578bca47938ba896b4d7
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-04 16:50:38 +0200

    ADD - Delete blog post and get post

--- src/server-actions/blog/blog.tsx
+++ src/server-actions/blog/blog.tsx
@@ -1,8 +1,13 @@
 'use server'
 import { ObjectId } from 'mongodb';
-import { getPointer, getMongoClinet } from './mongoClient'
+import { getPointer, getMongoClinet } from './../mongodb/mongoClient'
+import { revalidatePath } from 'next/cache';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation'
+import Token from '../utils/token';
 
 export interface BlogPost {
+  _id: ObjectId
   title: string,
   post: string,
   date: Date
@@ -29,11 +34,39 @@ export async function getBlogs() {
   }
 }
 
+export async function getBlog(id: string) {
+  try {
+    var mongoClient = await getMongoClinet()
+    await mongoClient.connect();
+    const pointer = await getPointer('blog');
+
+    const post = await pointer.findOne<BlogPost>({ _id: new ObjectId(id) });
+    return post;
+
+  } catch (error: Error | unknown) {
+    console.log(Error.toString());
+    throw new Error("Erro geting post");
+
+  } finally {
+    setTimeout(async () => {
+      await mongoClient.close();
+      console.log('Db closed');
+    }, 4000);
+  }
+}
+
 export async function postBlog(title: string, post: string) {
   try {
     var mongoClient = await getMongoClinet()
     await mongoClient.connect();
     const pointer = await getPointer('blog');
+
+    const cookie = cookies().get('ai')
+    const tokenObject = new Token()
+    const isValid = tokenObject.isValidToken(cookie?.value!)
+    if (!isValid) {
+      cookies().delete('ai')
+    }
 
     const newPost = {
       post,
@@ -42,6 +75,7 @@ export async function postBlog(title: string, post: string) {
     }
 
     const res = await pointer.insertOne(newPost)
+    revalidatePath("/dashboard")
     return res
 
   } catch (error: Error | unknown) {
@@ -54,5 +88,27 @@ export async function postBlog(title: string, post: string) {
       console.log('Db closed');
     }, 4000);
   }
+}
 
+export async function deletePost(id: ObjectId) {
+  try {
+    var mongoClient = await getMongoClinet()
+    await mongoClient.connect();
+    const pointer = await getPointer('blog');
+
+    console.log(id)
+    const res = await pointer.deleteOne({ _id: new ObjectId(id) })
+    revalidatePath("/dashboard")
+    return res
+
+  } catch (error: Error | unknown) {
+    console.log(Error.toString());
+    throw new Error("Error posting!");
+
+  } finally {
+    setTimeout(async () => {
+      await mongoClient.close();
+      console.log('Db closed');
+    }, 4000);
+  }
 }
