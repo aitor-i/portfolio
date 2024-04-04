bd7b8bcae4bcb1bf5be7fa6cc774e8edbf24f51f
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-04 16:50:51 +0200

    ADD - Login form functionality

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,41 +1,43 @@
 'use client'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { login } from '@/server-actions/login/login'
+import { useRouter } from 'next/navigation'
+
 
 export default function LoginForm() {
+  const [message, setMessage] = useState<string>()
+  const [loadingStatus, setLoadingStatus] = useState<'idle' | 'loading' | 'error' | 'success'>('idle')
+  const formRef = useRef<HTMLFormElement>(null)
+  const router = useRouter()
   const onSubmitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
+    setLoadingStatus('loading')
+
     const formData = new FormData(event.currentTarget)
-    const name = formData.get('name')?.valueOf();
-    const password = formData.get('password')?.valueOf();
+    const name = formData.get('username')?.toString();
+    const password = formData.get('password')?.toString();
 
-    const body = {
-      name,
-      password,
-    }
+    formRef.current?.reset()!
+    const res = await login(name!, password!)
+    if (!res) setMessage('Invalid username or password')
+    router.push("/dashboard")
+    setMessage('')
 
-    const url = new URL('http://localhost:3003/api/contact-form');
-
-    const response = await fetch(url, {
-      method: 'POST', // Set the method to POST
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body), // Convert the data to a JSON string
-    });
-
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
+    setLoadingStatus('success')
   }
 
   return (
-    <form className="grid gap-4" onSubmit={onSubmitHandler}>
-      <Input name='user' placeholder="Usuario" type="text" />
-      <Input name='password' placeholder="Contraseña" type='password' />
-      <Button className='bg-blue-500 hover:bg-blue-600' type="submit">Acceder</Button>
+    <form className="grid gap-4" ref={formRef} onSubmit={onSubmitHandler}>
+      <Input name='username' autoComplete='name' required placeholder="Usuario" type="text" />
+      <Input name='password' autoComplete='current-password' required placeholder="Contraseña" type='password' />
+      {message && <p className='text-sm text-red-500'>{message}</p>}
+      {loadingStatus === 'loading' ?
+        <div>Login...</div> :
+        <Button className='bg-blue-500 hover:bg-blue-600' type="submit">Acceder</Button>
+      }
     </form>
   )
 }
