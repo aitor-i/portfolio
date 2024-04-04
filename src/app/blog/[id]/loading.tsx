f07eaa8f8a69931deaaa5cce8331a46df14af5b8
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-04 18:57:33 +0200

    ADD - Loading post page

---
+++ src/app/blog/[id]/loading.tsx
@@ -0,0 +1,10 @@
+
+export default function Loading() {
+  return (
+    <section className="flex flex-col py-32 items-center  h-screen  ">
+      <div className="text-center space-y-4 flex flex-col gap-2">
+        <h1 className="text-6xl font-bold">Loading...</h1>
+      </div>
+    </section>
+  )
+}
