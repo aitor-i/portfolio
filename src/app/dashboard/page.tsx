b9f64e991e7a230c7bd27d0b4ed1f3d342aae3b3
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-04 16:48:07 +0200

    UPDATE - Dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,7 @@
 import Dashboard from "@/components/Dashboard/Dashboard";
-import Login from "@/components/Login/Login";
 
 export default function page() {
   return (
-    <>
-      <Login />
-      <Dashboard />
-    </>
+    <Dashboard />
   )
 }
