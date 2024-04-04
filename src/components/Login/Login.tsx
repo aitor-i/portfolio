a58d76613b7b2a170ab25ed7e959dc5252b76cbc
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-04 16:50:58 +0200

    UPDATE - Login styles

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -3,7 +3,7 @@ import LoginForm from '../LoginForm/LoginForm'
 
 export default function Login() {
   return (
-    <section className=''>
+    <section className='min-h-screen'>
       <div className='grid-cols-1 md:grid-cols-2 gap-4 md:gap-6 lg:gap-8 grid px-4 md:px-10 lg:px-24 py-8 md:py-48'>
         <div>
           <h1 className='text-4xl md:text-6xl font-bold '>Login</h1>
