4784f697bfce4635b22f7e986e8c50d740ea92ad
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-08 15:32:38 +0200

    UPDATE - Blog styles

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -7,8 +7,8 @@ export default async function page() {
   return (
     <section className="px-4 md:px-8 h-screen" >
       <h1 className="text-5xl md:text-6xl">Blog</h1>
-      <div className="grid gap-4 grid-cols-1  py-4 md:py-8">
-        {posts.map(post =>
+      <div className="grid gap-2 grid-cols-1  py-4 md:gap-4 md:py-8">
+        {posts.reverse().map(post =>
 
           <Link key={post._id.toString()} href={`blog/${post._id}`} className='p-4 bg-gray-50 text-gray-950 rounded' >
             <h3>{post.title}</h3>
