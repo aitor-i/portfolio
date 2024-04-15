1a088a19feb03c82c7b1fb61541fe03b8804963d
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-15 19:38:39 +0200

    UPDATE - Input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> { }
 
-const Input =
-  ({ className, type, ...props }: InputProps) => {
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
     return (
       <input
+        ref={ref}
         type={type}
         className={"flex text-gray-950 h-10 w-full rounded-md border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-500 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 " + className}
         {...props}
       />
     )
-  }
+  })
 
 
 export { Input }
