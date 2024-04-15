b6e87a24e30658b51fa04f7f61b06d8afd5fabba
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-15 19:38:27 +0200

    UPDATE - Blog styles

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default async function page() {
   const posts = await getBlogs()
   return (
-    <section className="px-4 md:px-8 h-screen" >
+    <section className="px-4 md:px-8 min-h-screen" >
       <h1 className="text-5xl md:text-6xl">Blog</h1>
       <div className="grid gap-2 grid-cols-1  py-4 md:gap-4 md:py-8">
         {posts.reverse().map(post =>
